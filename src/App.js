8284cb5a9cbbebe6a2518af97f54dc88c0aacda3
Blessedadudu <adududuke@gmail.com>
2020-04-30 09:09:53 +0100

    bar update

--- src/App.js
+++ src/App.js
@@ -9,48 +9,48 @@ class App extends Component {
     this.state = {
       chartData:{},
       price: [
-        {
-          "name": "Page A",
-          "uv": 4000,
-          "pv": 2400,
-          "amt": 2400
-        },
-        {
-          "name": "Page B",
-          "uv": 3000,
-          "pv": 1398,
-          "amt": 2210
-        },
-        {
-          "name": "Page C",
-          "uv": 2000,
-          "pv": 9800,
-          "amt": 2290
-        },
-        {
-          "name": "Page D",
-          "uv": 2780,
-          "pv": 3908,
-          "amt": 2000
-        },
-        {
-          "name": "Page E",
-          "uv": 1890,
-          "pv": 4800,
-          "amt": 2181
-        },
-        {
-          "name": "Page F",
-          "uv": 2390,
-          "pv": 3800,
-          "amt": 2500
-        },
-        {
-          "name": "Page G",
-          "uv": 3490,
-          "pv": 4300,
-          "amt": 2100
-        }
+        // {
+        //   "name": "Page A",
+        //   "uv": 4000,
+        //   "pv": 2400,
+        //   "amt": 2400
+        // },
+        // {
+        //   "name": "Page B",
+        //   "uv": 3000,
+        //   "pv": 1398,
+        //   "amt": 2210
+        // },
+        // {
+        //   "name": "Page C",
+        //   "uv": 2000,
+        //   "pv": 9800,
+        //   "amt": 2290
+        // },
+        // {
+        //   "name": "Page D",
+        //   "uv": 2780,
+        //   "pv": 3908,
+        //   "amt": 2000
+        // },
+        // {
+        //   "name": "Page E",
+        //   "uv": 1890,
+        //   "pv": 4800,
+        //   "amt": 2181
+        // },
+        // {
+        //   "name": "Page F",
+        //   "uv": 2390,
+        //   "pv": 3800,
+        //   "amt": 2500
+        // },
+        // {
+        //   "name": "Page G",
+        //   "uv": 3490,
+        //   "pv": 4300,
+        //   "amt": 2100
+        // }
       ]
     }
   }
@@ -90,10 +90,10 @@ class App extends Component {
       const response = JSON.parse(event.data);
       // console.log(response);
       this.setState({
-        // price: [{name: 'Page A', uv: 400, pv: 2400, amt: 2400}],
+        price: [...this.state.price, response],
         
       });
-      // console.log(this.state.price); 
+      console.log(this.state.price); 
 
     };
     ws.onclose = () => {
@@ -109,8 +109,8 @@ class App extends Component {
       <div className="App">
         {/* <Chart chartData={this.state.chartData} location="Massachusetts" legendPosition="bottom"/> */}
 
-        <AreaChart width={730} height={250} data={this.state.price}
-          margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
+        <AreaChart width={1000} height={500} data={this.state.price}
+          margin={{ top: 10, right: 30, left: 10, bottom: 10 }}>
           <defs>
             <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
               <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8}/>
@@ -121,12 +121,11 @@ class App extends Component {
               <stop offset="95%" stopColor="#82ca9d" stopOpacity={0}/>
             </linearGradient>
           </defs>
-          <XAxis dataKey="name" />
-          <YAxis />
-          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="sequence" />
+          <YAxis  dataKey="price" />
+          <CartesianGrid strokeDasharray="5 5" />
           <Tooltip />
-          <Area type="monotone" dataKey="uv" stroke="#8884d8" fillOpacity={1} fill="url(#colorUv)" />
-          <Area type="monotone" dataKey="pv" stroke="#82ca9d" fillOpacity={1} fill="url(#colorPv)" />
+          <Area type="monotone" dataKey="price" stroke="#8884d8" fillOpacity={1} fill="url(#colorUv)" />
         </AreaChart>
       </div>
     );
